b1da691ca85df90fcdbfb29237feb323e894be83
khaoula <mecherguikhaoula123@gmail.com>
2019-04-12 23:33:33 +0200

    'our_project'

---
+++ project/pages/accueil.php
@@ -0,0 +1,124 @@
+<! DOCTYPE HTML>
+<HTML>
+  <head>
+      <meta charset="utf-8">
+      <tit>Gestion des filiéres</tit>  
+       <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="../css/monstyle.css">
+      <link rel="stylesheet" type="text/css" href="../css/demo.css">
+      <link rel="stylesheet" type="text/css" href="../css/monstyle3.css">
+            <link rel="stylesheet" type="text/css" href="../css/monstyle2.css">
+
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  </head>
+    
+    
+    <body>
+        
+      <?php include ("menu.php"); ?>
+        <div   id="cont">
+                
+            <form method="post" action="lost.php" class="form">
+                
+                <button type="submit" class="btn" id="reg9" ><strong class="reg5">Inscrivez-vous</strong></button> 
+                </form>
+        
+                    </div>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+           <img src="../images/Capture1.PNG" alt="Chicago" style="width:100%;height:420px;">
+      </div>
+
+      <div class="item">
+        <img src="../images/Capture2.PNG" alt="Chicago" style="width:100%; height:420px;">
+      </div>
+    
+      <div class="item">
+        <img src="../images/Capture3.PNG" alt="New york" style="width:100%;height:420px;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+        
+ <footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Lost<span>Found</span></h3>
+
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Blog</a>
+					·
+					<a href="#">Pricing</a>
+					·
+					<a href="#">About</a>
+					·
+					<a href="#">Faq</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+
+				<p class="footer-company-name">Company Name &copy; 2015</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>21 Revolution Street</span> Paris, France</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+1 555 123456</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">support@company.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook-square"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+	                <a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+        
+        </body>
+
+</HTML>
---
+++ project/pages/menu.php
@@ -0,0 +1,39 @@
+
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="../css/monstyle.css">
+      <link rel="stylesheet" type="text/css" href="../css/demo.css">
+      <link rel="stylesheet" type="text/css" href="../css/monstyle3.css">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+<div class="container-fluid">
+    <div class="navbar-header">
+     <a href="../index.php" class="navbar-brand">Welcome in Lost    and Found things site in tunisia </a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="reg4" ><form class="form-inline" action="/action_page.php">
+    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <button class="btn btn-danger" type="submit">Search</button>
+  </form></li>
+     </ul>
+  </div>
+</nav>
+
+
+      <div  id="banniere_image">
+                
+            <form method="post" action="lost.php" class="form">
+                
+                <button type="submit" class="btn" id="reg1" ><strong class="reg3">LOST SOMETHING</strong></button> 
+                </form>
+                <form method="post" action="found.php" class="form">
+                <button type="submit" class="btn"id="reg2"  ><strong class="reg3">FOUND SOMETHING</strong></button> 
+               
+
+         </form>
+          
+        
+                        
+         
+    </div>
+       
+    
+
